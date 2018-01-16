2003904cbae4cf886c4e90a2d918c7d1d4daf460
koop4 <dilillo.roberto@gmail.com>
2018-01-16 12:32:37 +0100

    get json data from hitbtc
    	- since calls are made from the client, current browsers are probably gonna block them.
    	  Consider to add an header to allow any origin or just use the CORS chrome extension

--- js/hitbtc.js
+++ js/hitbtc.js
@@ -7,4 +7,11 @@ $(document).ready(function(){
 
 function getOrders() {
     console.log("Yo there, it's me, the button");
+    fetch('https://api.hitbtc.com/api/2/public/ticker')  
+        .then(transformToJson)
+        .then(logResponse);
+
+    /** internal functions */
+    function transformToJson(response) { return response.json(); }
+    function logResponse(response) { console.log(response); };
 }
